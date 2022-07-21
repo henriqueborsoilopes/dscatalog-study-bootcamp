7480214a4eb123a6c0fefb0c6f6cf74f9811e430
Henrique Borsoi Lopes <henriqueborsoilopes@gmail.com>
2022-07-21 19:53:19 -0300

    Update, Delete and Auditing data

--- backend/src/main/java/com/devsuperior/dscatalog/controllers/exceptions/ControllerExceptionHandler.java
+++ backend/src/main/java/com/devsuperior/dscatalog/controllers/exceptions/ControllerExceptionHandler.java
@@ -9,20 +9,35 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-import com.devsuperior.dscatalog.services.exceptions.EntityNotFoundException;
+import com.devsuperior.dscatalog.services.exceptions.ControllerNotFoundException;
+import com.devsuperior.dscatalog.services.exceptions.DatabaseException;
 
 @ControllerAdvice
 public class ControllerExceptionHandler {
 	
-	@ExceptionHandler(EntityNotFoundException.class)
-	public ResponseEntity<StandardError> entityNotFound(EntityNotFoundException e, HttpServletRequest request) {
+	@ExceptionHandler(ControllerNotFoundException.class)
+	public ResponseEntity<StandardError> entityNotFound(ControllerNotFoundException e, HttpServletRequest request) {
+		HttpStatus status = HttpStatus.NOT_FOUND;
 		StandardError err = new StandardError();
 		err.setTimestamp(Instant.now());
-		err.setStatus(HttpStatus.NOT_FOUND.value());
+		err.setStatus(status.value());
 		err.setError("Controller not found");
 		err.setMessage(e.getMessage());
 		err.setPath(request.getRequestURI());
 		
-		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
+		return ResponseEntity.status(status).body(err);
+	}
+	
+	@ExceptionHandler(DatabaseException.class)
+	public ResponseEntity<StandardError> entityNotFound(DatabaseException e, HttpServletRequest request) {
+		HttpStatus status = HttpStatus.BAD_REQUEST;
+		StandardError err = new StandardError();
+		err.setTimestamp(Instant.now());
+		err.setStatus(status.value());
+		err.setError("Database exception");
+		err.setMessage(e.getMessage());
+		err.setPath(request.getRequestURI());
+		
+		return ResponseEntity.status(status).body(err);
 	}
 }
--- backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -5,14 +5,19 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.dscatalog.dto.CategoryDTO;
 import com.devsuperior.dscatalog.entities.Category;
 import com.devsuperior.dscatalog.repositories.CategoryRespository;
-import com.devsuperior.dscatalog.services.exceptions.EntityNotFoundException;
+import com.devsuperior.dscatalog.services.exceptions.ControllerNotFoundException;
+import com.devsuperior.dscatalog.services.exceptions.DatabaseException;
 
 
 @Service
@@ -30,7 +35,7 @@ public class CategoryService {
 	@Transactional(readOnly = true)
 	public CategoryDTO findById(Long id) {
 		Optional<Category> optional = categoryRepository.findById(id);
-		Category category = optional.orElseThrow(() -> new EntityNotFoundException("Entity not found!"));
+		Category category = optional.orElseThrow(() -> new ControllerNotFoundException("Entity not found!"));
 		return new CategoryDTO(category);
 	}
 	
@@ -40,5 +45,30 @@ public class CategoryService {
 		category.setName(categoryDTO.getName());
 		category = categoryRepository.save(category);
 		return new CategoryDTO(category);
+	}
+
+	@Transactional
+	public CategoryDTO update(Long id, CategoryDTO categoryDTO) {
+		try {
+			Category category = categoryRepository.getOne(id);
+			category.setName(categoryDTO.getName());
+			category = categoryRepository.save(category);
+			return new CategoryDTO(category);
+		}
+		catch (EntityNotFoundException e) {
+			throw new ControllerNotFoundException("Id not found " + id);
+		}
+	}
+
+	public void delete(Long id) {
+		try {
+			categoryRepository.deleteById(id);
+		}
+		catch (EmptyResultDataAccessException e) {
+			throw new ControllerNotFoundException("Id not found " + id);
+		}
+		catch (DataIntegrityViolationException e) {
+			throw new DatabaseException("Integrity violation");
+		}
 	}
 }
