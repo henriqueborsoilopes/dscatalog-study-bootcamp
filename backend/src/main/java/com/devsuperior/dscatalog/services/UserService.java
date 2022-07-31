ccc78436a0b8c017b6f045d164aa0e2c4aac0a37
Henrique Borsoi Lopes <henriqueborsoilopes@gmail.com>
2022-07-30 21:05:57 -0300

    User CRUD

---
+++ backend/src/main/java/com/devsuperior/dscatalog/services/UserService.java
@@ -0,0 +1,98 @@
+package com.devsuperior.dscatalog.services;
+
+
+import java.util.Optional;
+
+import javax.persistence.EntityNotFoundException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.devsuperior.dscatalog.dto.RoleDTO;
+import com.devsuperior.dscatalog.dto.UserDTO;
+import com.devsuperior.dscatalog.dto.UserInsertDTO;
+import com.devsuperior.dscatalog.entities.Role;
+import com.devsuperior.dscatalog.entities.User;
+import com.devsuperior.dscatalog.repositories.RoleRepository;
+import com.devsuperior.dscatalog.repositories.UserRepository;
+import com.devsuperior.dscatalog.services.exceptions.ControllerNotFoundException;
+import com.devsuperior.dscatalog.services.exceptions.DatabaseException;
+
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Autowired
+	private RoleRepository roleRepository;
+	
+	@Autowired
+	private BCryptPasswordEncoder passwordEncoder;
+	
+	@Transactional(readOnly = true)
+	public Page<UserDTO> findAllPaged(Pageable pageable) {
+		Page<User> list = userRepository.findAll(pageable);
+		return list.map(x -> new UserDTO(x));
+	}
+	
+	@Transactional(readOnly = true)
+	public UserDTO findById(Long id) {
+		Optional<User> optional = userRepository.findById(id);
+		User user = optional.orElseThrow(() -> new ControllerNotFoundException("Entity not found!"));
+		return new UserDTO(user);
+	}
+	
+	@Transactional
+	public UserDTO insert(UserInsertDTO userInsertDTO) {
+		User user = new User();
+		copyDtoToEntity(userInsertDTO, user);
+		user.setPassword(passwordEncoder.encode(userInsertDTO.getPassword()));
+		user = userRepository.save(user);
+		return new UserDTO(user);
+	}
+
+	@Transactional
+	public UserDTO update(Long id, UserDTO userDTO) {
+		try {
+			User user = userRepository.getOne(id);
+			copyDtoToEntity(userDTO, user);
+			user = userRepository.save(user);
+			return new UserDTO(user);
+		}
+		catch (EntityNotFoundException e) {
+			throw new ControllerNotFoundException("Id not found " + id);
+		}
+	}
+
+	public void delete(Long id) {
+		try {
+			userRepository.deleteById(id);
+		}
+		catch (EmptyResultDataAccessException e) {
+			throw new ControllerNotFoundException("Id not found " + id);
+		}
+		catch (DataIntegrityViolationException e) {
+			throw new DatabaseException("Integrity violation");
+		}
+	}
+	
+	private void copyDtoToEntity(UserDTO userDTO, User user) {
+		user.setFirstName(userDTO.getFirstName());
+		user.setLastName(userDTO.getLastName());
+		user.setEmail(userDTO.getEmail());
+		
+		user.getRoles().clear();
+		for (RoleDTO roleDTO : userDTO.getRoles()) {
+			Role role = roleRepository.getOne(roleDTO.getId());
+			user.getRoles().add(role);
+		}
+	}
+}
